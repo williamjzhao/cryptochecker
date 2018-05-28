0dcb379289d67611c229fb17280b229877365642
williamjzhao <wzhao2017@gmail.com>
2018-05-28 15:28:17 -0500

    test

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var static = require('node-static');
 const express = require('express');
 const path = require('path');
-const { Client } = require('pg');
 
 var file = new static.Server();
 
@@ -11,17 +10,3 @@ require('http').createServer(function(request, response) {
   }).resume();
 }).listen(process.env.PORT || 3000);
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
