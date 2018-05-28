ea1cc8036186d72b9e82fc3224301ba1ce7320f2
williamjzhao <wzhao2017@gmail.com>
2018-05-28 15:19:15 -0500

    test

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const { Pool } = require('pg');
 
-
 var file = new static.Server();
 
 require('http').createServer(function(request, response) {
--- main.js
+++ main.js
@@ -175,22 +175,6 @@ function changeComparison(){
 }
 
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM crypto_prices');
-    res.render('pages/db', result);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
 
 
 
