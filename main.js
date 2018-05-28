69aefdb4b1fd7d20f64a8e24eca1823a1b344823
williamjzhao <wzhao2017@gmail.com>
2018-05-28 14:36:17 -0500

    testing

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-var static = require('node-static');
-var file = new static.Server();
-
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 3000);
-
 document.getElementById('sbutton').addEventListener('click',changeComparison);
 document.getElementById('cryptoinfo').style.display= "none";
 
